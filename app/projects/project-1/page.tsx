bd0704d2143cd22d0a4c0348169d4eb9ee4d2c8e
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-06-08 07:02:42 +0100

    Basic app router set up for Vercal testing

---
+++ app/projects/project-1/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPost1() {
+  return (
+    <main>
+      <h1>Blog Post 1</h1>
+      <p>This is the content of the first blog post.</p>
+    </main>
+  );
+}
---
+++ app/projects/project-2/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPost2() {
+  return (
+    <main>
+      <h1>Blog Post 2</h1>
+      <p>This is the content of the second blog post.</p>
+    </main>
+  );
+}
