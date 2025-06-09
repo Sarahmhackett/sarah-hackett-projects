8ed61d8aa68b8d5c5b32dfaa20e9724945ebd3ab
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-06-09 17:47:01 +0100

    About Page styling and content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,34 @@
+import styles from "./about.module.css";
+import { Barrio } from "next/font/google";
+import MyVerticalTimeline from "../components/Timeline";
+
+const googleFont = Barrio({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 export default function About() {
   return (
     <main>
-      <h1>This page would be about me</h1>
-      <p>WELP - I'm not very interesting...?</p>
+      <div className={styles.titleContainer}>
+        <h1 className={`${googleFont.className} ${styles.title}`}>
+          Sarah Hackett
+        </h1>
+      </div>
+
+      <div className={styles.introZone}>
+        <div className={styles.imageContainer}>IMAGE HERE - WIP PAGE</div>
+        <div className={styles.textContainer}>TEXT HERE - WIP PAGE</div>
+      </div>
+
+      <div className={styles.timelineContainer}>
+        <h1 className={`${googleFont.className} ${styles.timelineTitle}`}>
+          A series of fortunate events
+        </h1>
+        <div>
+          <MyVerticalTimeline />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ app/components/Timeline.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
+
+/* 
+Pink/red ACOTAR: #e64451
+green Must and Fury: #229880
+blue Frost and Starlight: #1a9fd4
+orange Silverflames: #f26c27
+grey Silverflames: #6e6c6d
+purple Wings and Ruin: #cb528b
+ */
+
+export default function MyVerticalTimeline() {
+  return (
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="Jun 2023 - current"
+        iconStyle={{ background: "#e64451", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">
+          Frontend Software Engineer
+        </h3>
+        <p className="vertical-timeline-element-subtitle">
+          Flutter Entertainment
+        </p>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2023"
+        iconStyle={{ background: "#229880", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">Code First Girls</h3>
+        <p className="vertical-timeline-element-subtitle">
+          Full Stack Degree bootcamp | Javascript Foundation | mySQL Foundation
+          courses
+        </p>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2021 - 2023"
+        iconStyle={{ background: "#1a9fd4", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">Business Analyst</h3>
+        <p className="vertical-timeline-element-subtitle">
+          Flutter Entertainment
+        </p>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2018"
+        iconStyle={{ background: "#f26c27", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">
+          CIM Chartered Instite of Marketing
+        </h3>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2014 - 2021"
+        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">Marketing Manager</h3>
+        <p className="vertical-timeline-element-subtitle">
+          <ul>
+            Manager roles:
+            <li>Proposition Marketing Manager</li>
+            <li>Commercial Marketing Manager</li>
+            <li>Game Content Marketing Manager</li>
+            Executive roles:
+            <li>Acquisiton & Media Executive</li>
+            <li>CRM Communications Executive</li>
+          </ul>
+        </p>
+      </VerticalTimelineElement>
+
+      {/* More elements if needed */}
+    </VerticalTimeline>
+  );
+}
